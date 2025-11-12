8e94631cf1d51a96c329bb6c76dfe22661c378b7
Adrian Dias <addias@c2r4s2.42lisboa.com>
2025-11-12 10:12:37 +0000

    gnl update

--- get_next_line.c
+++ get_next_line.c
@@ -4,44 +4,44 @@ int count_total_bytes(char *fileName)
 {
     int fd = open(fileName, O_RDONLY);
 
-    int size_file = 0;
+    int total_bytes = 0;
     int bytes_read;
     char buff[1024];
     
     while ((bytes_read = read(fd, buff, sizeof(buff))) > 0)
-        size_file += bytes_read;
-    printf("Total bytes of file: %d\n", size_file);
-    return (size_file);
+        total_bytes += bytes_read;
+    printf("Total bytes of file: %d\n", total_bytes);
+    return (total_bytes);
 }
 
-char* linelen(char *fileName, int size_file)
+int linelen(char *fileName, int total_bytes, char *buffer)
 {
     int fd = open(fileName, O_RDONLY);
     int len;
     int len_max;
-    char *buffer;
 
-    buffer = malloc(size_file + 1);
-    if(!buffer)
-        return(NULL);
     len = 0;
-    len_max = read(fd, buffer, size_file);
-    while(buffer[len] != '\n' && len < size_file)
+    len_max = read(fd, buffer, total_bytes);
+    while(buffer[len] != '\n' && len < total_bytes)
         len++;
     buffer[len] = '\0';
     // free(buffer);
     printf("LINE LEN: %d", len);
-    return(buffer);
+    return(len);
 }
 
 char *get_next_line(int fd, char *fileName)
 {
-    
+    char *buffer;
     int total_bytes = count_total_bytes(fileName);
-
-    printf("\nRETURNED LINE: %s", linelen(fileName, total_bytes));
-    return (fileName);
-
+    
+    buffer = malloc(total_bytes + 1);
+    if(!buffer)
+        return(NULL);
+    linelen(fileName, total_bytes, buffer);
+    
+    printf("\n%s", buffer);
+    return (buffer);
 }
 
 int main(void)
